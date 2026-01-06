26132e9be7e3773b8c86d7835976f2bfc199fc5a
Aurélie Fréchet <aurelie.frechet@gmail.com>
2026-01-06 18:20:21 +0100

    update tests get_SAS_proc

--- tests/testthat/test-get_SAS_procedure.R
+++ tests/testthat/test-get_SAS_procedure.R
@@ -11,7 +11,7 @@ test_that("PROC CHART", {
       "proc chart data=shirts; vbar size; title 'Number of Each Shirt Size Sold'; run;"
     ),
     list(proc = "chart",
-         contenu = "data=shirts; vbar size; title 'Number of Each Shirt Size Sold';")
+         contenu = c("data=shirts", "vbar size","title 'Number of Each Shirt Size Sold'"))
   )
 })
 
@@ -19,7 +19,7 @@ test_that("PROC CONTENTS", {
   expect_equal(
     get_SAS_procedure("proc contents data=sample; run;"),
     list(proc = "contents",
-         contenu = "data=sample;")
+         contenu = "data=sample")
   )
 
 })
@@ -28,7 +28,7 @@ test_that("PROC FREQ", {
   expect_equal(
     get_SAS_procedure(text = "proc freq data = example1; tables y; run;"),
     list(proc = "freq",
-         contenu = "data = example1; tables y;")
+         contenu = c("data = example1","tables y"))
   )
 })
 
@@ -38,7 +38,7 @@ test_that("PROC GCHART", {
       "proc gchart data=totals; format sales dollar8.; block site / sumvar=sales; run; quit;"
     ),
     list(proc = "gchart",
-         contenu = "data=totals; format sales dollar8.; block site / sumvar=sales;")
+         contenu = c("data=totals","format sales dollar8.","block site / sumvar=sales"))
   )
 })
 
@@ -48,7 +48,7 @@ test_that("PROC MEANS", {
   expect_equal(
     get_SAS_procedure(text = "proc means data=sashelp.cars q1 median q3; var msrp invoice; run;"),
   list(proc = "means",
-       contenu = "data=sashelp.cars q1 median q3; var msrp invoice;")
+       contenu = c("data=sashelp.cars q1 median q3", "var msrp invoice"))
   )
 })
 
@@ -57,7 +57,7 @@ test_that("PROC PLOT", {
     get_SAS_procedure(
   "proc plot data=djia; plot high*year='*' / vspace=5 vaxis=by 1000; title 'High Values of the Dow Jones Industrial Average'; title2 'from 1968 to 2008'; run;"),
   list(proc = "plot",
-       contenu = "data=djia; plot high*year='*' / vspace=5 vaxis=by 1000; title 'High Values of the Dow Jones Industrial Average'; title2 'from 1968 to 2008';")
+       contenu = c("data=djia","plot high*year='*' / vspace=5 vaxis=by 1000","title 'High Values of the Dow Jones Industrial Average'","title2 'from 1968 to 2008'"))
   )
   })
 
@@ -67,7 +67,7 @@ test_that("PROC SQL", {
     get_SAS_procedure(
   "proc sql; select * from sashelp.cars where Type='Sports' and MSRP>100000; run;"),
   list(proc = "sql",
-       contenu = "select * from sashelp.cars where Type='Sports' and MSRP>100000;")
+       contenu = "select * from sashelp.cars where Type='Sports' and MSRP>100000")
   )
 })
 

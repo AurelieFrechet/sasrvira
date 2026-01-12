1429924157e8aa52c23de7711e8b069df72494fb
Aurélie Fréchet <aurelie.frechet@gmail.com>
2026-01-12 11:30:16 +0100

    remove dependencies from sasr_contents and sasr_libname

--- R/sasr_contents.R
+++ R/sasr_contents.R
@@ -7,8 +7,8 @@
 #'
 sasr_contents <- function(code_sas) {
 
-  code_r <- data_equal_to(code_sas) %>%
-    paste_str(.)
+  code_r <- data_equal_to(code_sas) |>
+    paste_str()
 
   return(code_r)
 }

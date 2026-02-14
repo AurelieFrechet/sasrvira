4b45d575d5a4db983314482f220041b3e5747aa7
Aurélie Fréchet <aurelie.frechet@gmail.com>
2026-02-14 10:08:55 +0100

    delete deprecated file update documentation

--- R/utils.R
+++ R/utils.R
@@ -1,7 +1,18 @@
-#' data_equal_to
-#' @description detecte la valeur da data et la renvoie, contenur dans une
-#' fonction file.path si elle est associée à une librairie SAS
-#' @param sas_code character: code sas
+#' Extract DATA= target from SAS code
+#'
+#' @description
+#' Detects the value assigned to a SAS \code{DATA=} statement and returns it.
+#' If the dataset is qualified with a SAS library (e.g. \code{lib.dataset}),
+#' it is converted into an R-compatible \code{file.path()} expression.
+#'
+#' @param sas_code Character string containing SAS code.
+#'
+#' @return
+#' A character string representing the dataset name. If a SAS library is
+#' specified, the result is a \code{file.path()} call combining the library
+#' and dataset name.
+#'
+#' @keywords internal
 data_equal_to <- function(sas_code){
   data_equal <- regex_match_groups(x = sas_code,
             pattern = "data\\s?=\\s?([0-9a-zA-Z._]+)")[[1]]

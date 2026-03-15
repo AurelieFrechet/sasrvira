9a4f4f010a7d3670bc30c6a868632d7346a7121d
Aurélie Fréchet <aurelie.frechet@gmail.com>
2026-03-15 10:21:02 +0100

    Dataspecs ready to use

--- R/class_DataSpecs.R
+++ R/class_DataSpecs.R
@@ -4,25 +4,41 @@ DataSpecs <- new_class(
     data  = S7::class_character,
     where = S7::class_character,
     keep  = S7::class_character,
-    drop  = S7::class_character
+    drop  = S7::class_character,
+    rename = S7::class_character,
+    firstobs = S7::class_numeric,
+    obs      = S7::class_numeric
   )
   ,
   constructor = function(data_statement) {
     data_statement <- regex_replace(data_statement, "\\s+", " ")
-   data_info <- regex_match_groups(
-     data_statement,
-      pattern = "([A-z0-9.]+)\\s?\\((drop|keep|where)\\s?=\\s?(.*)\\)"
-      )
+    data_info <- regex_match_groups(
+      data_statement,
+      pattern = "([A-z0-9.]+)\\s?(\\((.*)\\))"
+    )
 
-   check_statement <- function(x) if(data_info[[2]]==x) data_info[[3]] else character(0)
+    dataspecs <- data_info[[3]]
 
-   new_object(
-     .parent = S7_object(),
-     data  = data_info[[1]],
-     where = regex_remove(check_statement("where"), "\\(|\\)"),
-     keep  = splitws(check_statement("keep")),
-     drop  = splitws(check_statement("drop"))
-   )
+    splitted_specs <- split_sql_query(dataspecs,
+                    keywords = c("drop", "keep", "where", "rename", "firstobs", "obs",
+                                 "in", "end", "idxname", "idxwhere", "point", "index", # not used
+                                 "pw", "read", "write", "alter", "encrypt", "compress", #not used
+                                 "label", "genmax", "gennum")) # not used
+
+    splitted_specs$text <- regex_remove(x = splitted_specs$text, pattern = "^=") |> trimws()
+
+    .extract_spec <- function(x) splitted_specs$text[(splitted_specs$key_word == x)]
+
+    new_object(
+      .parent = S7_object(),
+      data  = data_info[[1]],
+      where = regex_remove(.extract_spec("where"), "^\\(|\\)$"),
+      keep  = splitws(.extract_spec("keep")),
+      drop  = splitws(.extract_spec("drop")),
+      rename = regex_remove(.extract_spec("rename"), "^\\(|\\)$"),
+      firstobs = as.numeric(.extract_spec("firstobs")),
+      obs      = as.numeric(.extract_spec("obs"))
+    )
   }
 )
 
@@ -34,6 +50,15 @@ transpile_data_specs <- S7::new_generic("transpile_data_specs", "x")
 S7::method(transpile_data_specs, DataSpecs) <- function(x) {
   data_output <- x@data
 
+  ## filter ----
+  if(!identical(x@where, character(0))){
+    where_as_filter <- x@where |>
+      transform_conditions() |>
+      transform_functions() |>
+      paste_function(function_name = "filter", content = _)
+
+    data_output <- paste(data_output, where_as_filter, sep = " %>%\n\t")
+  }
   ## select ----
   keep_as_select <- NULL
   drop_as_select <- NULL
@@ -51,16 +76,34 @@ S7::method(transpile_data_specs, DataSpecs) <- function(x) {
     select_statement <- paste_function(function_name = "select",
                                   content = paste(select_statement, collapse = ", "))
 
-    data_output <- paste(data_output, select_statement, sep = " %>%\n")
+    data_output <- paste(data_output, select_statement, sep = " %>%\n\t")
   }
 
-  ## filter ----
-  if(!identical(x@where, character(0))){
-  where_as_filter <- x@where |>
-    transform_conditions() |>
-    paste_function(function_name = "filter", content = _)
+  ## rename ----
+  if (!identical(x@rename, character(0))) {
+  rename_statement <- x@rename |>
+    transform_functions() |>
+    regex_replace("\\s+?=\\s+?", "=") |>
+    regex_match_groups("(\\w+=\\w+)")
 
-  data_output <- paste(data_output, where_as_filter, sep = " %>%\n")
+  old_new <- strsplit(rename_statement[[1]], split = "=")
+  rename_old <- sapply(old_new, '[[', 1)
+  rename_new <- sapply(old_new, '[[', 2)
+
+  rename_statement <-  paste_function(function_name = "rename",
+                                      content = paste(paste(rename_new, "=", rename_old), collapse = ", "))
+
+  data_output <- paste(data_output, rename_statement, sep = " %>%\n\t")
+  }
+
+  ## slice ----
+  if (!identical(x@obs, numeric(0))) {
+    slice_value <- x@obs
+    if (!identical(x@firstobs, numeric(0))) {
+      slice_value <- paste(x@firstobs, x@obs, sep = ":")
+    }
+    slice_statement <-  paste_function(function_name = "slice", slice_value)
+    data_output <- paste(data_output, slice_statement, sep = " %>%\n\t")
   }
 
   return(data_output)
--- tests/testthat/test-class_DataSpecs.R
+++ tests/testthat/test-class_DataSpecs.R
@@ -5,7 +5,7 @@ test_that("Where clause =", {
   expect_equal(test@where, "product='whizmo'")
   expect_equal(test@keep, character(0))
   expect_equal(test@drop, character(0))
-  expect_equal(transpile_data_specs(test), "sales %>%\nfilter(product == 'whizmo')")
+  expect_equal(transpile_data_specs(test), "sales %>%\n\tfilter(product == 'whizmo')")
 })
 
 
@@ -17,7 +17,7 @@ test_that("keep clause", {
   expect_equal(test@keep, c("name", "weight", "height"))
   expect_equal(test@drop, character(0))
 
-  expect_equal(transpile_data_specs(test), "sashelp.class %>%\nselect(name, weight, height)")
+  expect_equal(transpile_data_specs(test), "sashelp.class %>%\n\tselect(name, weight, height)")
 })
 
 test_that("drop clause", {
@@ -28,6 +28,72 @@ test_that("drop clause", {
   expect_equal(test@keep, character(0))
   expect_equal(test@drop, c("name", "weight", "height"))
 
-  expect_equal(transpile_data_specs(test), "sashelp.class %>%\nselect(-name, -weight, -height)")
+  expect_equal(transpile_data_specs(test), "sashelp.class %>%\n\tselect(-name, -weight, -height)")
 })
 
+test_that("multi clauses", {
+  data_statement <- "sashelp.class (drop=name  keep=weight height where = (height ge 14))"
+  test <- DataSpecs(data_statement)
+  expect_equal(test@data, "sashelp.class")
+  expect_equal(test@where, "height ge 14")
+  expect_equal(test@keep, c("weight", "height"))
+  expect_equal(test@drop, c("name"))
+
+  expect_equal(transpile_data_specs(test), "sashelp.class %>%\n\tfilter(height >= 14) %>%\n\tselect(weight, height, -name)")
+})
+
+test_that("unexcepted clause", {
+  data_statement <- "sashelp.class (label='something' drop=name weight height)"
+  test <- DataSpecs(data_statement)
+  expect_equal(test@data, "sashelp.class")
+  expect_equal(test@where, character(0))
+  expect_equal(test@keep, character(0))
+  expect_equal(test@drop, c("name", "weight", "height"))
+
+  expect_equal(transpile_data_specs(test), "sashelp.class %>%\n\tselect(-name, -weight, -height)")
+})
+
+
+# Transpile only ----------------------------------------------------------
+
+
+
+test_that("order output", {
+  data_statement <- "mydata(
+    keep=id age salary
+    rename=(salary=income)
+    where=(salary>50000)
+)"
+  test <- DataSpecs(data_statement)
+
+  expect_equal(transpile_data_specs(test), "mydata %>%\n\tfilter(salary > 50000) %>%\n\tselect(id, age, salary) %>%\n\trename(income = salary)")
+})
+
+test_that("firstobs and obs", {
+  data_statement <- "mydata(firstobs=10 obs=20)"
+  test <- DataSpecs(data_statement)
+
+  expect_equal(transpile_data_specs(test), "mydata %>%\n\tslice(10:20)")
+})
+
+
+test_that("where with function", {
+  data_statement <- "mydata(where=(upcase(sex)=\"F\"))"
+  test <- DataSpecs(data_statement)
+
+  expect_equal(transpile_data_specs(test), "mydata %>%\n\tfilter(toupper(sex) == \"F\")")
+})
+
+test_that("where with missing", {
+  data_statement <- "mydata(where=(age ne .))"
+  test <- DataSpecs(data_statement)
+
+  expect_equal(transpile_data_specs(test), "mydata %>%\n\tfilter(!is.na(age))")
+})
+
+test_that("rename multiple", {
+  data_statement <- "mydata(rename=(age=age_years salary=income))"
+  test <- DataSpecs(data_statement)
+
+  expect_equal(transpile_data_specs(test), "mydata %>%\n\trename(age_years = age, income = salary)")
+})
